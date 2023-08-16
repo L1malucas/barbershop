1aa5d77feaf005f4e6585e1c0e8237c364b7be84
Lucas Lima <lucaslima.94@hotmail.com.br>
2023-08-16 20:41:20 -0300

    feature: animated splash screen

--- app/lib/src/features/splash/splash_page.dart
+++ app/lib/src/features/splash/splash_page.dart
@@ -1,14 +1,58 @@
 import 'package:flutter/material.dart';
 
-class SplashPage extends StatelessWidget {
+class SplashPage extends StatefulWidget {
+  const SplashPage({super.key});
 
-  const SplashPage({ super.key });
+  @override
+  State<SplashPage> createState() => _SplashPageState();
+}
 
-   @override
-   Widget build(BuildContext context) {
-       return Scaffold(
-           appBar: AppBar(title: const Text('splash'),),
-           body: Container(),
-       );
+class _SplashPageState extends State<SplashPage> {
+  double _scale = 10.0;
+  double _animationOpacityLogo = 0.0;
+  double get _logoAnimationWidth => 100 * _scale;
+  double get _logoAnimationHeight => 120 * _scale;
+  @override
+  void initState() {
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      setState(() {
+        _scale = 1;
+        _animationOpacityLogo = 1;
+      });
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DecoratedBox(
+        decoration: const BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage(
+              'assets/images/background_image_chair.jpg',
+            ),
+            fit: BoxFit.cover,
+          ),
+        ),
+        child: Center(
+          child: AnimatedOpacity(
+            curve: Curves.easeIn,
+            opacity: _animationOpacityLogo,
+            duration: const Duration(seconds: 3),
+            child: AnimatedContainer(
+              width: _logoAnimationWidth,
+              height: _logoAnimationHeight,
+              curve: Curves.linearToEaseOut,
+              duration: const Duration(seconds: 3),
+              child: Image.asset(
+                'assets/images/imgLogo.png',
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
